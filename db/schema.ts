7d4ec9618123eb971936e823d3ef0fdaf95c8f99
thebadams <bpeteradams@gmail.com>
2024-06-22 21:41:05 -0400

    database: more work fleshing out coffee schema

--- db/schema.ts
+++ db/schema.ts
@@ -3,4 +3,6 @@ import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
 export const coffees = pgTable("coffees", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 256 }),
+  roaster: varchar("roaster", { length: 256 }),
+  origin: varchar("origin", { length: 256 }),
 });
